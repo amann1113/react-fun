50738e7f04375ffc8f735f4c7df5153c25df8d07
Austin Mann <austin.mann1113@gmail.comgit config --global user.name Austin>
2021-02-25 09:05:21 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App text = 'Coding is fun'/>, document.getElementById('root'));
